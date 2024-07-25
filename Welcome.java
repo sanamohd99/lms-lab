42a348a81526c332d529e0adf5acc8a5d525c2a2
sanamohd99 <140255705+sanamohd99@users.noreply.github.com>
2024-07-25 16:37:03 +0530

    Add files via upload

---
+++ Welcome.java
@@ -0,0 +1,16 @@
+public class Welcome {
+    public static void main (String args[]){
+        System.out. println(" Computers are electronic devices it is invented by  Charles Babbage");
+        System.out.println("They are 4 types they are supercomputer, main frame , mini, personal computer");
+        System.out.println("A computer has four main components: a cpu ,a graphics processing unit, random access memory or a solid state drive");
+        System.out.println("Computers are used in many sectors");
+        System.out.println("they can be operated with internet and without operated");
+        System.out.println("The cpu is the brain of the computer");
+        System.out.println("We use keyboard and mouse to instruct the computer to perform input and output operations");
+        System.out.println("In computer we can store the data of any organization to certain task to be performed");
+        System.out.println("In this era computers are playing very vital in daily life");
+        System.out.println("Computer come in many shapes and sizes and they perform many different functions in our daily lives example withdraw from an atm, scan groceries at the store");
+        System.out.println(" it is  used to perform the calculation");
+
+    }
+}
