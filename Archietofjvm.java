6bc00d8bc0418c72ee7fa2e0289707d9cca05b80
sanamohd99 <140255705+sanamohd99@users.noreply.github.com>
2024-07-31 18:54:15 +0530

    Add files via upload

---
+++ Archietofjvm.java
@@ -0,0 +1,29 @@
+public class Archietofjvm {
+    public static void main(String[] args) {
+        System.out.println("Java is a programming lanuage originally developed by James Gosling at Sun Mirosystems.");
+        System.out.println("Java application are typically compiled to byte code class file that " +
+                "can run on any java virtual machine regardless of computer architecture. ");
+        System.out.println("The Hotspot Jvm, developed b Oracle, is the most widely used jvm implementation");
+        System.out.println("The java virtual machine jvm  ia an abstract computing machine ");
+        System.out.println("Jvm enables java programs to run on any device or operating system");
+        System.out.println("It interprets compiled java bytecode, allowing developers to write" +
+                " code once and run it anywhere");
+        System.out.println("Jvm is platform dependent and are available for many hardware and software");
+        System.out.println("It is mainly responsible for three activities, Loading, Linking, Initialization.");
+        System.out.println("Jvm is divided into sereval runtime data areas, including the heap, stack, and method area");
+        System.out.println("The heap is where objects are stored");
+        System.out.println("The stack contains method frames and local variables.");
+        System.out.println("The method area holds clas-level information like metadata, constants ,and static variable");
+        System.out.println("It several key functions, including clas loading,memory management," +
+                " and garbage collection");
+        System.out.println("It ensures security by running code within a controlled environment, " +
+                "preventing unauthorized access");
+        System.out.println("Jvm also supports Just-In-Time jit compilation which enhances " +
+                "performance by translating bytecode into native machine code at runtime");
+        System.out.println("The Jvm's probability and efficiency have made" +
+                " it a popular choice for building scalable and high-performance applications");
+
+
+
+    }
+}
---
+++ Intro.java
@@ -0,0 +1,21 @@
+public class Intro {
+    public static void main(String[] args) {
+        /* -------intstructing to display this as output-------*/
+        System.out.println("Hello, I'm Sana ");
+        System.out.println("I'm from Hyderabad ");
+        System.out.println("I'm pursing Mca 1st year in Avanthi group of institution in Hyderabad");
+        System.out.println("I've done Bachelor of science in Mathematics, Statistics, Computerscience ");
+        System.out.println("I'm currently learning java to know more how it is practically implemented");
+        System.out.println("I've participated in quiz completion which was done organized by our college");
+        System.out.println("I belong to nuclear family");
+        System.out.println("My interest are:");
+        System.out.println("Travelling , I feel that travelling is the therapy to explore new thing");
+        System.out.println("Surfing through the internet");
+        System.out.println("Reading, listening music, watching movies");
+        System.out.println("Playing games indoor and outdoor");
+
+
+
+    }
+
+}
